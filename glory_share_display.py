91d644c774be7975b0d13f3c27af99f317740300
benny29b-server <54730591+benny29b-server@users.noreply.github.com>
2020-02-28 21:30:25 +0900

    Add files via upload

---
+++ glory_share_display.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+import discord
+import random
+ 
+token = 'NjgyOTI3MDcwMjMzNjI0NjA0.XlkHgA.I-ry2zv9e2Z2fmEUriwgx6JOYcI'
+
+bot = commands.Bot(command_prefix='!')
+ 
+@bot.event
+async def on_ready():
+    print('logged in as \nname: {}\n  id: {}'.format(bot.user.name, bot.user.id))
+    print('='*80)
+    
+    await bot.change_presence(game=discord.Game(name='화면공유', type=1))
+ 
+@bot.command(pass_context=True)
+async def share(ctx):
+    voice_channel = ctx.message.author.voice.voice_channel
+
+    voice_channel_id = voice_channel.id
+
+    url = 'https://discordapp.com/channels/611294722501378048/'+str(voice_channel_id)
+
+    await bot.say('{}'.format(url))
+
+bot.run(token)
